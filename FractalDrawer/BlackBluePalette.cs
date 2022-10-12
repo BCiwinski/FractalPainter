85e30504569b3af06a0dacd288f7f779b3250b1f
BCiwinski <114406887+BCiwinski@users.noreply.github.com>
2022-10-12 13:12:55 +0200

    extract a general abstract class ColorPointPalette from BlackBluePalette

--- FractalDrawer/BlackBluePalette.cs
+++ FractalDrawer/BlackBluePalette.cs
@@ -8,61 +8,19 @@ using FractalPainterInterfaces;
 
 namespace FractalDrawer
 {
-    public class BlackBluePalette : IFractalColorPalette
+    public class BlackBluePalette : ColorPointPalette
     {
-        List<ColorItr> colorItr = new List<ColorItr>() {
-        new ColorItr {color = Color.Black, itr = 0},
-        new ColorItr {color = Color.DarkBlue, itr = 20},
-        new ColorItr {color = Color.Blue, itr = 50},
-        new ColorItr {color = Color.Black, itr = 100}
-        };
-
-        public int IterationsMax { get{ return 100;} }
-
-        private float stretch = 1.0f;
-        public float Stretch { get { return stretch; } set { stretch = value; } }
-
-        public Color GetIterationColor(int iteration)
+        protected override void setupColorPoints()
         {
-            ColorItr colorBefore = colorItr.First();
-            ColorItr colorAfter = colorItr.Last();
-
-
-            foreach (ColorItr ci in colorItr)
-            {
-                if (ci.itr * stretch <= iteration && colorBefore.itr * stretch < ci.itr * stretch)
-                    colorBefore = ci;
-
-                if (ci.itr * stretch >= iteration && colorAfter.itr * stretch > ci.itr * stretch)
-                    colorAfter = ci;
-            }
-
-            if (colorAfter.itr == colorBefore.itr)
-                return colorBefore.color;
-
-
-            int range = (int)((colorAfter.itr - colorBefore.itr) * stretch);
-            int colorPos = (int)(iteration - colorBefore.itr * stretch);
-
-            float colorAMix = (float)colorPos / (float)range;
-            float colorBMix = 1 - colorAMix;
-
-            Color result = Color.FromArgb(
-                255,
-                (int)(colorAfter.color.R * colorAMix + colorBefore.color.R * colorBMix),
-                (int)(colorAfter.color.G * colorAMix + colorBefore.color.G * colorBMix),
-                (int)(colorAfter.color.B * colorAMix + colorBefore.color.B * colorBMix)
-                );
-
-            return result;
+            colorItr = new List<ColorItr>() {
+                new ColorItr {color = Color.Black, itr = 0},
+                new ColorItr {color = Color.DarkBlue, itr = 20},
+                new ColorItr {color = Color.Blue, itr = 50},
+                new ColorItr {color = Color.Black, itr = 100}
+                };
         }
-
-
     }
 
-    struct ColorItr
-    {
-        public Color color;
-        public int itr;
-    }
+
+
 }
---
+++ FractalDrawer/ColorPointPalette.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using FractalPainterInterfaces;
+
+namespace FractalDrawer
+{
+    public abstract class ColorPointPalette : IFractalColorPalette
+    {
+        protected List<ColorItr> colorItr;
+
+        public int IterationsMax { get { return 100; } }
+
+        private float stretch = 1.0f;
+        public float Stretch { get { return stretch; } set { stretch = value; } }
+
+        public ColorPointPalette()
+        {
+            setupColorPoints();
+        }
+
+        abstract protected void setupColorPoints();
+
+        public Color GetIterationColor(int iteration)
+        {
+            ColorItr colorBefore = colorItr.First();
+            ColorItr colorAfter = colorItr.Last();
+
+
+            foreach (ColorItr ci in colorItr)
+            {
+                if (ci.itr * stretch <= iteration && colorBefore.itr * stretch < ci.itr * stretch)
+                    colorBefore = ci;
+
+                if (ci.itr * stretch >= iteration && colorAfter.itr * stretch > ci.itr * stretch)
+                    colorAfter = ci;
+            }
+
+            if (colorAfter.itr == colorBefore.itr)
+                return colorBefore.color;
+
+
+            int range = (int)((colorAfter.itr - colorBefore.itr) * stretch);
+            int colorPos = (int)(iteration - colorBefore.itr * stretch);
+
+            float colorAMix = (float)colorPos / (float)range;
+            float colorBMix = 1 - colorAMix;
+
+            Color result = Color.FromArgb(
+                255,
+                (int)(colorAfter.color.R * colorAMix + colorBefore.color.R * colorBMix),
+                (int)(colorAfter.color.G * colorAMix + colorBefore.color.G * colorBMix),
+                (int)(colorAfter.color.B * colorAMix + colorBefore.color.B * colorBMix)
+                );
+
+            return result;
+        }
+
+
+    }
+
+    public struct ColorItr
+    {
+        public Color color;
+        public int itr;
+    }
+}
